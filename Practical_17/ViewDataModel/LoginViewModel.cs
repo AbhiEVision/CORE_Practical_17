555db815c512f6605e641e60addf2c4b01a636e3
Abhi Patel <abhi.d@simformsolutions.com>
2023-07-06 17:33:05 +0530

    add  : view models

---
+++ Practical_17/ViewDataModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practical_17.ViewDataModel
+{
+	public class LoginViewModel
+	{
+		[Key]
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public required string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
+	}
+}
---
+++ Practical_17/ViewDataModel/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practical_17.ViewDataModel
+{
+	public class RegisterViewModel
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public required string Name { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DataType(DataType.EmailAddress)]
+		public required string Email { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "Both Password Must be same")]
+		public required string ConfirmPassword { get; set; }
+
+
+	}
+}
