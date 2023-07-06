09dcb313a94bd6a7442a95c16b45566451250f37
Abhi Patel <abhi.d@simformsolutions.com>
2023-07-06 17:33:48 +0530

    add : other common stuffs

--- Practical_17/Program.cs
+++ Practical_17/Program.cs
@@ -1,53 +1,66 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Practical_17.Data;
+using Practical_17.Interfaces;
+using Practical_17.Repositories;
 
 namespace Practical_17
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			//builder.Services.AddControllersWithViews();
+            //builder.Services.AddControllersWithViews();
 
-			builder.Services.AddMvc();
+            builder.Services.AddMvc();
 
-			builder.Services.AddDbContext<DatabaseContext>(options =>
-			{
-				options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-			});
+            builder.Services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
 
-			builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-			{
-				options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
-				options.AccessDeniedPath = "/Account/AccessDenied";
-				options.LoginPath = "/Account/Login";
-				options.LogoutPath = "/Account/Logout";
-			});
 
-			var app = builder.Build();
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+            });
 
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				app.UseHsts();
-			}
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-			app.UseRouting();
+            var app = builder.Build();
 
-			app.UseAuthorization();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}"
-			);
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.Run();
-		}
-	}
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+        }
+    }
 }
